507b897f7ddcb8a08feff82fbcb49bfdfcedb63a
anuprshetty <anuprshetty@gmail.com>
2017-04-02 07:38:10 +0530

    id generation in mongodb

--- pymongo_module/main.py
+++ pymongo_module/main.py
@@ -8,3 +8,16 @@ cluster = MongoClient('mongodb://127.0.0.1:27017/?appname=PyMongo%20Client') # H
 db = cluster["company"]
 collection = db["users"]
 
+# _id:
+# - Each document/post is uniquely identified by _id.
+# - If you don't specify _id, MongoDB will generate a unique random value for _id.
+# - Ex: _id: ObjectId("60f158b76e5d66174224b9aa")
+
+# - Without _id
+post = {"name": "tim", "age": 27, "email": "tim@gmail.com"}
+# - With _id
+post0 = {"_id": 0, "name": "cook", "age": 35, "email": "cook@gmail.com"}
+post1 = {"_id": 1, "name": "ram", "age": 35, "email": "ram@yahoo.com"}
+post2 = {"_id": 2, "name": "sham", "age": 46, "email": "sham@gmail.com"}
+post3 = {"_id": 3, "name": "bob", "age": 46, "email": "bob@yahoo.com"}
+
