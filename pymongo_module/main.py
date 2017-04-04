89a80abd9e7c04ad082d8f087bd541f403fb281f
anuprshetty <anuprshetty@gmail.com>
2017-04-04 17:26:42 +0530

    mongodb insertion operation

--- pymongo_module/main.py
+++ pymongo_module/main.py
@@ -21,3 +21,18 @@ post1 = {"_id": 1, "name": "ram", "age": 35, "email": "ram@yahoo.com"}
 post2 = {"_id": 2, "name": "sham", "age": 46, "email": "sham@gmail.com"}
 post3 = {"_id": 3, "name": "bob", "age": 46, "email": "bob@yahoo.com"}
 
+print('Insertion:')
+print("-------------------------------------------------")
+
+print('- Inserting one document/post at a time: ')
+insert_one_result = collection.insert_one(post)
+print('inserted_id: {}'.format(insert_one_result.inserted_id))
+insert_one_result = collection.insert_one(post0)
+print('inserted_id: {}'.format(insert_one_result.inserted_id))
+print("-------------------------------------------------")
+
+print('- Inserting multiple documents/posts at a time: ')
+insert_many_result = collection.insert_many([post1, post2, post3])
+print('inserted_ids: {}'.format(insert_many_result.inserted_ids))
+print("-------------------------------------------------")
+
