cfdd8ddf852fc45aa81596394b13af4b1682c75c
anuprshetty <anuprshetty@gmail.com>
2017-04-07 19:47:55 +0530

    mongodb searching operation

--- pymongo_module/main.py
+++ pymongo_module/main.py
@@ -36,3 +36,50 @@ insert_many_result = collection.insert_many([post1, post2, post3])
 print('inserted_ids: {}'.format(insert_many_result.inserted_ids))
 print("-------------------------------------------------")
 
+print('Searching:')
+print("-------------------------------------------------")
+
+print('- Search for documents in a collection by single field: ')
+# - .find()
+cursor = collection.find({"age": 35})
+# - .find() returns a cursor.
+# - Cursor is a MongoDB Collection of the documents which is returned upon the find method execution.
+print(cursor)
+print("-------------------------------------------------")
+
+for document in cursor:
+    print(document)
+    for key, value in document.items():
+        print('{} : {}'.format(key, value))
+    print("-------------------------------------------------")
+
+print('- Search for documents in a collection by multiple field: ')
+# - .find_one()
+document = collection.find_one({"age": 35, "name": "ram"})
+print(document)
+print("-------------------------------------------------")
+
+print('- Search for documents in a collection using regex: ')
+# - .find()
+cursor = collection.find({"email": {'$regex': 'gmail.com$'}})
+print(cursor)
+print("-------------------------------------------------")
+
+for document in cursor:
+    print(document)
+    for key, value in document.items():
+        print('{} : {}'.format(key, value))
+    print("-------------------------------------------------")
+
+print('- Search for all documents in a collection: ')
+# cursor = collection.find({})
+cursor = collection.find()
+print(cursor)
+print("-------------------------------------------------")
+
+for document in cursor:
+    print(document)
+    for key, value in document.items():
+        print('{} : {}'.format(key, value))
+    print("-------------------------------------------------")
+
